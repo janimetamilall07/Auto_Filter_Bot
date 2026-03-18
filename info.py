96c85416f4141e6be29af4aee25516464efaee9f
Jcodeflix.org <janimetamilall@gmail.com>
2026-03-18 07:56:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,13 +32,13 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 COVERX = bool(environ.get('COVERX', True)) # Use cover image for indexed files (default: True)
 # If you disable it then bot will use a default thumb for all files
 
-PICS_URL = (environ.get('PICS', 'https://api.aniwallpaper.workers.dev/random?type=girl')).split() #random anime girl img each time from aniwallpaper (Experimental)
+PICS_URL = (environ.get('PICS', 'https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE').split() #random anime girl img each time from aniwallpaper (Experimental)
 PICS = (environ.get('PICS', 'https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE')).split()  # Sample pic
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/242b7f1b52743938d81f1.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
+NOR_IMG = environ.get("NOR_IMG", "https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEMBpppugwNIod607-aSV1kHYJ6CMByCQACZg1rG_fW0FWD8v0jqvUKhAEAAwIAA3kAAzoE")
+MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE")
+SPELL_IMG = environ.get("SPELL_IMG", "https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE'))
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAEL_aRpsPFeYzT9RY-ZwSwMYwSerQRkDgACJA1rG-xoiFVIIIc4sKhVQQEAAwIAA3kAAzoE')).split()  # Fsub pic
 
 # ============================
 # Admin, Channels & Users Configuration
