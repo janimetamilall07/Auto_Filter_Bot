b8674a12260b71bb0e8ff3a84128fb8b0ac0395f
Jcodeflix.org <janimetamilall@gmail.com>
2026-03-18 20:52:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,9 +81,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collec
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "
-
-mongodb+srv://janimetamilall:9489473162@cluster01.jqzkr09.mongodb.net/?appName=Cluster01")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://janimetamilall:9489473162@cluster01.jqzkr09.mongodb.net/?appName=Cluster01")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
 # ============================
 # Movie Notification & Update Settings
 # ============================
